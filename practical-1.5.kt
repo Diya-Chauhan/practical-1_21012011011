d5d96c9150e99dade9ab53f1047922a780f4ea55
student <student@gnu.ac.in>
2023-07-17 08:52:06 +0530

    nkink

--- practical-1.5.kt
+++ practical-1.5.kt
@@ -6,8 +6,19 @@ fun main(){
         1->"January"
         2->"February"
         3->"March"
-        4->April
+        4->"April"
+        5->"May"
+        6->"June"
+        7->"July"
+        8->"August"
+        9->"September"
+        10->"October"
+        11->"November"
+        12->"December"
+        else->"Enter a valid month"
     }
+
+    println(result)
 
 
 
