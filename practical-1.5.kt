2e372cb534af7ef2493cdcaad847cee08787a997
student <student@gnu.ac.in>
2023-07-17 08:25:02 +0530

    nkink

---
+++ practical-1.5.kt
@@ -0,0 +1,15 @@
+fun main(){
+    print("Enter a month number: ")
+    val monthnum = readLine()!!.toInt()
+    val result=when (monthnum)
+    {
+        1->"January"
+        2->"February"
+        3->"March"
+        4->April
+    }
+
+
+
+
+}
