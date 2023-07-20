e28b8c6e4eae3a8205bf805090b0a11ddb3f8be7
student <student@gnu.ac.in>
2023-07-20 08:53:26 +0530

    iki

---
+++ practical-1.8.kt
@@ -0,0 +1,18 @@
+import java.util.Arrays
+
+fun main()
+{
+    var a= arrayOf(43,12,11,65,33)
+    println(Arrays.deepToString(a))
+    var b=IntArray(5)
+    b[0]=10
+    b[1]=20
+    b[2]=30
+    b[3]=40
+    b[4]=50
+    for(i in 0 until b.size){
+        print("Array using Intarra
+
+    }
+
+}
